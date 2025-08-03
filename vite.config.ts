3a8effd60afa9c0581da6085ea6a0ea4075e5667
wat <citrus.juno@gmail.com>
2025-08-04 01:26:38 +0900

    fix: vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 export default defineConfig({
+  base: "/metaverse-project/", // ← 追加
   plugins: [vue()],
   resolve: {
     alias: {
