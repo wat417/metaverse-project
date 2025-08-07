73f69255e39195bc69c9b711be29f94f119f4f31
wat <citrus.juno@gmail.com>
2025-08-08 02:05:47 +0900

    20250807_作業中24

--- copy404.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-const source = path.join(__dirname, "dist", "chartTestPage.html");
-const destination = path.join(__dirname, "dist", "404.html");
-
-fs.copyFileSync(source, destination);
-console.log("404.html copied from chartTestPage.html");
